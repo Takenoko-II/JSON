cd2b70d4d0f8bc8dd15ca2b9b5e6994cf5d653c3
たけのこII <yu2.aoki0229.@gmail.com>
2025-09-19 17:49:43 +0900

    c

--- src/test/java/com/gmail/takenokoii78/Main.java
+++ src/test/java/com/gmail/takenokoii78/Main.java
@@ -3,5 +3,6 @@ package com.gmail.takenokoii78;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // TODO: ValueType::equals, hashCode
     }
 }
