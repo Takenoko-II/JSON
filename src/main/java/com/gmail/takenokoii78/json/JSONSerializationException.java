41f5f6d83659e96490d65117f887690e13a06b54
たけのこII <yu2.aoki0229.@gmail.com>
2025-10-07 23:22:48 +0900

    protected

--- src/main/java/com/gmail/takenokoii78/json/JSONSerializationException.java
+++ src/main/java/com/gmail/takenokoii78/json/JSONSerializationException.java
@@ -3,15 +3,11 @@ package com.gmail.takenokoii78.json;
 import org.jetbrains.annotations.NotNull;
 
 public class JSONSerializationException extends RuntimeException {
-    public JSONSerializationException() {
-        super();
-    }
-
-    public JSONSerializationException(@NotNull String message) {
+    protected JSONSerializationException(@NotNull String message) {
         super(message);
     }
 
-    public JSONSerializationException(@NotNull String message, @NotNull Throwable cause) {
+    protected JSONSerializationException(@NotNull String message, @NotNull Throwable cause) {
         super(message, cause);
     }
 }
